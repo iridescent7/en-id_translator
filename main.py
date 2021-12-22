fcc4e7d41386f09afbf160636cd72cfbaf10e1ce
Anthony Kevin Oktavius <programist851@gmail.com>
2021-12-22 21:19:52 +0700

    added get_batch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import torch
 
 from dataset import Dataset
 
@@ -18,4 +19,29 @@ if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
 
-def train_model()
+def get_batch(source, i):
+    sequence_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+sequence_len]
+    target = source[i+1:i+1+sequence_len].reshape(-1)
+
+    return data, target
+
+def train_model(model):
+    loss = nn.CrossEntropyLoss()
+    learning_rate = 5.0
+    optimizer = torch.optim.Adam(model.parameters(), learning_rate=learning_rate)
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, 
+        1.0, 
+        gamma=0.95
+    )
+
+    model.train()
+    total_loss = 0
+    log_interval = 200
+    start_time = generate_square_subsequent_mask(bptt).to(device)
+
+    num_batches = len(train_data) // bptt
+
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_
