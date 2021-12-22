3b16fb6c596742785755884d253ec175da091e23
Anthony Kevin Oktavius <programist851@gmail.com>
2021-12-22 21:52:26 +0700

    added train_model

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_batch(source, i):
     return data, target
 
 def train_model(model):
-    loss = nn.CrossEntropyLoss()
+    cross_entropy_loss = nn.CrossEntropyLoss()
     learning_rate = 5.0
     optimizer = torch.optim.Adam(model.parameters(), learning_rate=learning_rate)
     scheduler = torch.optim.lr_scheduler.StepLR(
@@ -44,4 +44,28 @@ def train_model(model):
     num_batches = len(train_data) // bptt
 
     for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
-        data, targets = get_
+        data, targets = get_batch(train_data, i)
+        batch_size = data.size(0)
+        if batch_size != bptt:
+            source_mask = source_mask[:batch_size, :batch_size]
+        output = model(data, source_mask)
+        loss = cross_entropy_loss(output.view(-1, ntokens), taregts)
+
+        optimizer.zero_grad()
+        cross_entropy_loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+
+        optimizer.step()
+
+        total_loss += cross_entropy_loss.item()
+        if batch  % log_interval == 0 and batch > 0:
+            learning_rate = scheduler.get_last_lr()[0]
+            cur_loss = total_loss / log_interval
+
+            print(
+                f'| epoch {epoch} | 
+                {batch}/{num_batches} | 
+                learning rate: {learning_rate}',
+            )
+            total_loss = 0
+            
